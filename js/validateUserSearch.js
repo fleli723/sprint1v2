aabe0501f3c20cc013bd2b5fcc5000af02c166d5
Vidaillet <cvida526@uwsp.edu>
2019-10-26 13:06:15 -0500

    Adding to js branch, I have the javascript working for both the survey and the search bar for the ablums

---
+++ js/validateUserSearch.js
@@ -0,0 +1,36 @@
+//This functin will evalute what the user entered
+
+let ValidateUserSearch = function() {
+	
+    event.preventDefault();
+    let has_User_Entered_A_Search = false;
+    
+
+    //Checking what the user typed in the search box 
+   // let userSearch = document.forms["myForm"]["SearchBar"].value; 
+   
+   
+	let userSearch = document.forms["userSearchBarForm"]["SearchBar"].value;
+	
+	
+	
+	
+	
+
+    if (userSearch == "") 
+	{
+        alert("Please Enter your search Pretty Please");
+        return false;
+    } else {
+        has_User_Entered_A_Search = true;
+    }
+
+     
+   
+
+    if (has_User_Entered_A_Search) {
+		
+        window.location.href = "../views/searchResults.php";
+    }
+
+}//end validateForm function
