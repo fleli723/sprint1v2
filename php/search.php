13f400e3d043320b5a2699e8fcffadf0d0012919
Lelis <fleli723@uwsp.edu>
2019-11-03 19:31:56 -0600

    fixed code structuring

--- php/search.php
+++ php/search.php
@@ -11,17 +11,7 @@ $page->finalizeBottomSection();
 
 print $page->getTopSection();
 
-print	'
-<div class="topbar">
-	<h1> CNMT 310 Sprint 1 Assignment</h1>
-	<ul class="nav">
-		<li><a href="index.php">Home</a></li>		
-		<li><a href="survey.php">Survey</a></li>
-		<li><a href="privacy.php">Privacy Policy</a></li>
-		<li><a href="search.php">Search</a></li>
-	</ul>
-</div>
-';
+include("topNavBar.php");
 
 print	'
 <div class="content">
